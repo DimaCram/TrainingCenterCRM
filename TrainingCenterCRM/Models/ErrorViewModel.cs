284178d129962c8005587cfe64752706da881a0e
Dmitrij <kramkov@ncpi.gov.by>
2021-07-26 10:27:12 +0300

    Добавьте файлы проекта.

---
+++ TrainingCenterCRM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrainingCenterCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
