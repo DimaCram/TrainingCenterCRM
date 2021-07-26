f06ec1da5d8decdd16fddab5d0cbac875625eed2
Dmitrij <kramkov@ncpi.gov.by>
2021-07-26 13:45:22 +0300

    added functionality for working with BLL

---
+++ TrainingCenterCRM.BLL/Interfaces/IStudentService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrainingCenterCRM.BLL.DTO;
+
+namespace TrainingCenterCRM.BLL.Interfaces
+{
+    public interface IStudentService
+    {
+        void AddStudent(StudentDTO studentDTO);
+        void EditStudent(StudentDTO studentDTO);
+        void DeleteStudent(int id);
+        StudentDTO GetStudent(int id);
+        IEnumerable<StudentDTO> GetStudents();
+        void Dispose();
+    }
+}
---
+++ TrainingCenterCRM.BLL/Services/StudentService.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TrainingCenterCRM.BLL.DTO;
+using TrainingCenterCRM.BLL.Interfaces;
+using TrainingCenterCRM.DAL;
+using TrainingCenterCRM.DAL.Enttities;
+using TrainingCenterCRM.DAL.Interfaces;
+
+namespace TrainingCenterCRM.BLL.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IUnitOfWork db;
+
+        public StudentService()
+        {
+            db = new UnitOfWork();
+        }
+        public void AddStudent(StudentDTO studentDTO)
+        {
+            if (studentDTO == null)
+                throw new ArgumentException();
+
+            var student = new Student()
+            {
+                Id = studentDTO.Id,
+                Name = studentDTO.Name,
+                Surname = studentDTO.Surname,
+                Age = studentDTO.Age
+            };
+
+            db.Students.Create(student);
+            db.Save();
+        }
+        public void EditStudent(StudentDTO studentDTO)
+        {
+            if (studentDTO == null)
+                throw new ArgumentException();
+
+            var student = new Student()
+            {
+                Id = studentDTO.Id,
+                Name = studentDTO.Name,
+                Surname = studentDTO.Surname,
+                Age = studentDTO.Age
+            };
+
+            db.Students.Update(student);
+            db.Save();
+        }
+        public void DeleteStudent(int id)
+        {
+            db.Students.Delete(id);
+            db.Save();
+        }
+        public StudentDTO GetStudent(int id)
+        {
+            var student = db.Students.Get(id);
+            return new StudentDTO()
+            {
+                Id = student.Id,
+                Name = student.Name,
+                Surname = student.Surname,
+                Age = student.Age
+            };
+        }
+        public IEnumerable<StudentDTO> GetStudents()
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Student, StudentDTO>()).CreateMapper();
+            return mapper.Map<IEnumerable<Student>, List<StudentDTO>>(db.Students.GetAll());
+        }
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
