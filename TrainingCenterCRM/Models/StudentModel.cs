84cef54173865b83f7698ccb7910ba70097e1276
Dmitrij <kramkov@ncpi.gov.by>
2021-07-27 12:42:03 +0300

    added display name for group id

--- TrainingCenterCRM/Models/StudentModel.cs
+++ TrainingCenterCRM/Models/StudentModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace TrainingCenterCRM.Models
         public string Surname { get; set; }
         public int Age { get; set; }
 
+        [Display(Name = "Group name")]
         public int GroupId { get; set; }
         public GroupModel Group { get; set; }
     }
