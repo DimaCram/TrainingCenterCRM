9a436539f197a6c9e38ea09b7da54d43f64f89bd
Dmitrij <kramkov@ncpi.gov.by>
2021-07-30 15:44:23 +0300

    added Teacher for BLL

---
+++ TrainingCenterCRM.BLL/DTO/TeacherDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingCenterCRM.BLL.DTO
+{
+    public class TeacherDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Bio { get; set; }
+
+        public GroupDTO Group { get; set; }
+        public int GroupId { get; set; }
+    }
+}
---
+++ TrainingCenterCRM.BLL/Interfaces/ITeacherService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrainingCenterCRM.BLL.DTO;
+using TrainingCenterCRM.DAL.Enttities;
+
+namespace TrainingCenterCRM.BLL.Interfaces
+{
+    public interface ITeacherService
+    {
+
+        void AddTeacher(TeacherDTO teacherDTO);
+        void EditTeacher(TeacherDTO teacherDTO);
+        void DeleteTeacher(int id);
+        TeacherDTO GetTeacher(int id);
+        List<Teacher> GetTeachers();
+        void Dispose();
+    }
+}
