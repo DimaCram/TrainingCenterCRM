fc761b31e2f9afb8f0955a6ea8d90fe33af9cef6
Dmitrij <kramkov@ncpi.gov.by>
2021-08-03 14:39:05 +0300

    added validation

--- TrainingCenterCRM/Models/StudentModel.cs
+++ TrainingCenterCRM/Models/StudentModel.cs
@@ -9,12 +9,20 @@ namespace TrainingCenterCRM.Models
     public class StudentModel
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Surname { get; set; }
+
+        [Required]
         public int Age { get; set; }
 
+        [Required]
         [Display(Name = "Group name")]
         public int GroupId { get; set; }
+        
         public GroupModel Group { get; set; }
     }
 }
