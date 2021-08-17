7bc564af830ac1cb7a0b513c2e6295a423a149ee
Cram <kramkovdd@gmail.com>
2021-08-17 11:25:41 +0300

    added account models and changed views

---
+++ TrainingCenterCRM/Models/Account/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingCenterCRM.Models.Account
+{
+    public class RegisterModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
