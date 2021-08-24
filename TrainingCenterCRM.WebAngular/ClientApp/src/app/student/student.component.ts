83f9c158f89fa507a0ea6c053504e09cd59c32bc
Dmitrij <kramkov@ncpi.gov.by>
2021-08-24 11:05:10 +0300

    added students

---
+++ TrainingCenterCRM.WebAngular/ClientApp/src/app/student/student.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, Inject } from "@angular/core";
+
+@Component({
+    selector: 'app-student',
+    templateUrl: './student.component.html'
+  })
+  export class StudentComponent {
+    public students: Student[];
+
+    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+      http.get<Student[]>(baseUrl + 'students').subscribe(result => {
+        this.students = result;
+        console.log(result);
+      }, error => console.error(error));
+    }
+  }
+
+  interface Student {
+    id: number;
+    name: string;
+    surname: string;
+    age: number;
+  }
