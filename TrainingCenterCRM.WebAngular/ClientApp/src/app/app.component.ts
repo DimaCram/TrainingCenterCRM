db765b971c74a1150f7995a26b178338f70db132
Dmitrij <kramkov@ncpi.gov.by>
2021-08-31 12:16:36 +0300

    added aos

--- TrainingCenterCRM.WebAngular/ClientApp/src/app/app.component.ts
+++ TrainingCenterCRM.WebAngular/ClientApp/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app';
+
+  ngOnInit() {
+    AOS.init();
+  }
+
 }
