6504bba9eb2d526c2b8183c209c40dc2b88e55d0
Dmitrij <kramkov@ncpi.gov.by>
2021-08-31 14:12:40 +0300

    added for every component style file

--- TrainingCenterCRM.WebAngular/ClientApp/src/app/courses/courses.component.ts
+++ TrainingCenterCRM.WebAngular/ClientApp/src/app/courses/courses.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-courses',
-    templateUrl: './courses.component.html'
+    templateUrl: './courses.component.html',
+    styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent{
 
--- TrainingCenterCRM.WebAngular/ClientApp/src/app/teachers/teachers.component.ts
+++ TrainingCenterCRM.WebAngular/ClientApp/src/app/teachers/teachers.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-teachers',
-    templateUrl: './teachers.component.html'
+    templateUrl: './teachers.component.html',
+    styleUrls: ['./teachers.component.css']
 })
 
 export class TeachersComponent{
