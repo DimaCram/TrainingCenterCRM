69f2d0543533ac89193c2c5a2ef7f21160352571
Dmitrij <kramkov@ncpi.gov.by>
2021-09-17 14:57:46 +0300

    add core

--- TrainingCenterCRM.Api/Program.cs
+++ TrainingCenterCRM.Api/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TrainingCenterCRM.WebApi
+namespace TrainingCenterCRM.Api
 {
     public class Program
     {
