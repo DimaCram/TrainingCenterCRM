53a2f2d8b35d4c829cf1d935265f50401ad42046
Dmitriy Kramkov <kramkovdd@gmail.com>
2021-11-10 21:21:41 +0300

    minor fix

--- TrainingCenterCRM.Api/Controllers/StudentMarksController.cs
+++ TrainingCenterCRM.Api/Controllers/StudentMarksController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,6 +12,7 @@ using TrainingCenterCRM.Core.Models;
 
 namespace TrainingCenterCRM.Api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class StudentMarksController : ControllerBase
